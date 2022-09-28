968fbfd485843ab843f679c594e48fe55f95ab2b
Hugo Cadenat <h.cadenat@gmail.com>
2022-09-28 14:58:40 +0200

    Paramétrage route auth

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { restart } = require('nodemon');
 
 const userRoutes = require('./routes/user');
 
@@ -15,10 +14,12 @@ mongoose.connect('mongodb+srv://hcdnt:KdYYjVt2Hg5JcXUD@cluster0.sf5l5zq.mongodb.
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((request, response, next) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
@@ -26,13 +27,16 @@ app.use((request, response, next) => {
 
 app.use('/api/auth', userRoutes);
 
-app.post('/api/user', (req, res, next) => {
-    const user = new User({
-        ...req.body
-    });
-    user.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
+
+// INUTILE
+
+// app.post('/api/auth', () => {
+//     const user = new User({
+//         ...req.body
+//     });
+//     user.save()
+//         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+//         .catch(error => res.status(400).json({ error }));
+// });
 
 module.exports = app;
