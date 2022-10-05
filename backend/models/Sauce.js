685bea1fa0a7c77feb0ae4ca685e1457810e8478
Hugo Cadenat <h.cadenat@gmail.com>
2022-10-05 12:39:28 +0200

    Test remontée dossier images sur github

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+// const uniqueValidator = require('mongoose-unique-validator');
 
 const sauceSchema = mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    name: { type: String, required: true, unique: true },
+    // userId: { type: String, required: true, unique: true },
+    userId: { type: String, required: true },
+    // name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     manufacturer: { type: String, required: true },
-    description: { type: String, required: true, unique: true },
+    // description: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
     mainPepper: { type: String, required: true, },
-    imageUrl: { type: String, required: true, unique: true },
+    // imageUrl: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
     heat: { type: Number, required: true, },
     // likes: { type: Number, required: true, },
     // dislikes: { type: Number, required: true, },
